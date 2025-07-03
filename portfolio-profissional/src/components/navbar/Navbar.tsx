70f25c8fc7329e5fac861f7abc863763f54ba786
Pedro <pedro.ribeiro33@fatec.sp.gov.br>
2025-07-03 18:15:41 -0300

    feat: initial navigation

--- portfolio-profissional/src/components/navbar/Navbar.tsx
+++ portfolio-profissional/src/components/navbar/Navbar.tsx
@@ -1,13 +1,16 @@
-import styles from  './Navbar.module.css';
+import { useNavigate } from 'react-router-dom';
+import styles from './Navbar.module.css';
 
 function Navbar() {
+  const navigate = useNavigate();
+
   return (
     <div className={styles.container}>
 
       <div className={styles.botoes}>
-        <button className={styles.botao}>Home</button>
+        <button className={styles.botao} onClick={() => navigate('/')}>Home</button>
         <button className={styles.botao}>Sobre mim</button>
-        <button className={styles.botao}>Projetos</button>
+        <button className={styles.botao} onClick={() => navigate('/projetos')}>Projetos</button>
         <button className={styles.botao}>Formações</button>
       </div>
 
